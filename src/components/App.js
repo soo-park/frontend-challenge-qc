1d425df7090575916e828de435fc9cbb412a6bda
soo-park <sparkstacks@gmail.com>
2017-10-08 19:04:21 -0700

    comments deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,6 @@
 import React, { Component } from 'react';
 import Card from './Card.js';
 
-/* 
-
-Usually, the code will have more components to make the eniter app modulized.
-However, Codepen was requested to be used to submit the code.
-Since Codepen allows only 10 files for free account unless you pay annual fee,
-the following list of things are adjusted to accomodate:
-
-- Components and helper functions were consolidated to fit into the limitation.
-- Redux, Thunk, SCSS could not be used because they need more files
-- README.md could not be generated (thus, these comment lines)
-- Since Codepen does not allow any media only one SVG image was used
-- The mock api server file generated was substituted with hard-coded data
-
-*/
-
-
 class App extends Component {
   constructor(props) {
     super(props);
