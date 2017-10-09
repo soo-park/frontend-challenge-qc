b816ac066a42bca45bb8e7c80dbd0a07e17837d9
soo-park <sparkstacks@gmail.com>
2017-10-08 18:42:45 -0700

    README updated

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ Since Codepen allows only 10 files for free account unless you pay annual fee,
 the following list of things are adjusted to accomodate:
 
 - Components and helper functions were consolidated to fit into the limitation.
-- Redux, Thunk, Sass could not be used because they need more files
+- Redux, Thunk, SCSS could not be used because they need more files
 - README.md could not be generated (thus, these comment lines)
 - Since Codepen does not allow any media only one SVG image was used
 - The mock api server file generated was substituted with hard-coded data
